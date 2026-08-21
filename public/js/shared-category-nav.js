2c951859da4033d22fdb772112b8d5f7bcdcdbec
s106 <88272270+Sasan1376@users.noreply.github.com>
2026-08-21 01:15:03 -0700

    Add mobile accessories category navigation

--- public/js/shared-category-nav.js
+++ public/js/shared-category-nav.js
@@ -52,6 +52,17 @@
       seeAllHref: '/mobiles',
     },
     {
+      id: 'mobile-accessories',
+      name: 'لوازم جانبی موبایل',
+      icon: 'ti-headphones',
+      links: [
+        { label: 'لوازم جانبی اپل', href: '/mobiles?category=%D9%84%D9%88%D8%A7%D8%B2%D9%85%20%D8%AC%D8%A7%D9%86%D8%A8%DB%8C%20%D9%85%D9%88%D8%A8%D8%A7%DB%8C%D9%84&brand=%D8%A7%D9%BE%D9%84' },
+        { label: 'لوازم جانبی سامسونگ', href: '/mobiles?category=%D9%84%D9%88%D8%A7%D8%B2%D9%85%20%D8%AC%D8%A7%D9%86%D8%A8%DB%8C%20%D9%85%D9%88%D8%A8%D8%A7%DB%8C%D9%84&brand=%D8%B3%D8%A7%D9%85%D8%B3%D9%88%D9%86%DA%AF' },
+        { label: 'لوازم جانبی شیائومی', href: '/mobiles?category=%D9%84%D9%88%D8%A7%D8%B2%D9%85%20%D8%AC%D8%A7%D9%86%D8%A8%DB%8C%20%D9%85%D9%88%D8%A8%D8%A7%DB%8C%D9%84&brand=%D8%B4%DB%8C%D8%A7%D8%A6%D9%88%D9%85%DB%8C' },
+      ],
+      seeAllHref: '/mobiles?category=%D9%84%D9%88%D8%A7%D8%B2%D9%85%20%D8%AC%D8%A7%D9%86%D8%A8%DB%8C%20%D9%85%D9%88%D8%A8%D8%A7%DB%8C%D9%84',
+    },
+    {
       id: 'tablet',
       name: 'تبلت',
       icon: 'ti-device-tablet',
