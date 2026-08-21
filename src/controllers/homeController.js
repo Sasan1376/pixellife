775971ccc3a0351c54a35d71e4a5a4bbb09f74c1
s106 <88272270+Sasan1376@users.noreply.github.com>
2026-08-21 01:53:26 -0700

    Prevent demo products on accessory pages

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -254,7 +254,7 @@ function sendAccessoryBrandView(res, brand) {
       )
       .replace(
         "/js/database-catalog.js?v=20260820-card-design-10",
-        "/js/database-catalog.js?v=20260821-accessories-1",
+        "/js/database-catalog.js?v=20260821-accessories-2",
       );
     html = injectEnamad(html);
     html = injectSharedCategoryNav(html);
